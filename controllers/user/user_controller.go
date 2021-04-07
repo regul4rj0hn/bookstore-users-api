e083bbdf6d9a4cf0003bd8cc68c00f5a7d6b7ef6
Mauro Lavigna <mauro.lavigna@gmail.com>
2021-04-07 19:09:14 -0300

    add basic user create format

--- controllers/user/user_controller.go
+++ controllers/user/user_controller.go
@@ -1,13 +1,28 @@
 package user
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/regul4rj0hn/bookstore-users-api/domain/users"
+	"github.com/regul4rj0hn/bookstore-users-api/services"
 )
 
 func Create(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "Not implemented")
+	var user users.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	result, saveErr := services.Create(user)
+	if saveErr != nil {
+		fmt.Println(saveErr)
+		return
+	}
+
+	c.JSON(http.StatusCreated, result)
 }
 
 func Get(c *gin.Context) {
--- services/users_service.go
+++ services/users_service.go
@@ -1,1 +1,7 @@
 package services
+
+import "github.com/regul4rj0hn/bookstore-users-api/domain/users"
+
+func Create(user users.User) (*users.User, error) {
+	return &user, nil
+}
