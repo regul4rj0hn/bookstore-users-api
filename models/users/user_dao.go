22c153eaeb18f8c907571313d87110d940a31c00
Mauro Lavigna <mauro.lavigna@gmail.com>
2021-04-19 17:43:52 -0300

    use database on user create dao

--- models/users/user_dao.go
+++ models/users/user_dao.go
@@ -2,10 +2,16 @@ package users
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/regul4rj0hn/bookstore-users-api/data/psql/users"
 	"github.com/regul4rj0hn/bookstore-users-api/utils/dates"
 	"github.com/regul4rj0hn/bookstore-users-api/utils/errors"
+)
+
+const (
+	constraintUniqueEmail = "user_email_key"
+	queryInsertUser       = "INSERT INTO public.user (first_name, last_name, email, created_on) VALUES ($1, $2, $3, $4) RETURNING id;"
 )
 
 var (
@@ -32,16 +38,14 @@ func (user *User) Get() *errors.Response {
 }
 
 func (user *User) Save() *errors.Response {
-	current := usersDB[user.Id]
-	if current != nil {
-		if current.Email == user.Email {
-			return errors.Conflict(fmt.Sprintf("email %s already registered", user.Email))
+	user.CreatedOn = dates.GetNowString()
+	err := users.DB.QueryRow(queryInsertUser, user.FirstName, user.LastName, user.Email, user.CreatedOn).Scan(&user.Id)
+	if err != nil {
+		if strings.Contains(err.Error(), constraintUniqueEmail) {
+			return errors.Conflict(fmt.Sprintf("email %s is already registered", user.Email))
 		}
-		return errors.Conflict(fmt.Sprintf("user %d already exists", user.Id))
+		return errors.InternalServerError(err.Error())
 	}
 
-	user.CreatedOn = dates.GetNowString()
-
-	usersDB[user.Id] = user
 	return nil
 }
