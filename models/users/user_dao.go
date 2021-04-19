83eda5395ca0b9c328e02138fb62af00b62ecf55
Mauro Lavigna <mauro.lavigna@gmail.com>
2021-04-19 18:52:04 -0300

    improve psql error handling a bit

--- models/users/user_dao.go
+++ models/users/user_dao.go
@@ -1,24 +1,19 @@
 package users
 
 import (
-	"fmt"
-	"strings"
-
 	"github.com/regul4rj0hn/bookstore-users-api/data/psql/users"
 	"github.com/regul4rj0hn/bookstore-users-api/utils/dates"
 	"github.com/regul4rj0hn/bookstore-users-api/utils/errors"
+	"github.com/regul4rj0hn/bookstore-users-api/utils/postgres"
 )
 
 const (
-	errorEmptyResult = "no rows in result set"
-	errorUniqueEmail = "user_email_key"
-
+	querySelectUser = "SELECT * FROM public.user WHERE id = $1"
 	queryInsertUser = "INSERT INTO public.user (first_name, last_name, email, created_on) VALUES ($1, $2, $3, $4) RETURNING id;"
-	queryGetUser    = "SELECT * FROM public.user WHERE id = $1"
 )
 
 func (user *User) Get() *errors.Response {
-	stmt, err := users.DB.Prepare(queryGetUser)
+	stmt, err := users.DB.Prepare(querySelectUser)
 	if err != nil {
 		return errors.InternalServerError(err.Error())
 	}
@@ -26,10 +21,7 @@ func (user *User) Get() *errors.Response {
 
 	result := stmt.QueryRow(user.Id)
 	if err := result.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.CreatedOn); err != nil {
-		if strings.Contains(err.Error(), errorEmptyResult) {
-			return errors.NotFound(fmt.Sprintf("user %d not found", user.Id))
-		}
-		return errors.InternalServerError(err.Error())
+		return postgres.ParseError(err)
 	}
 
 	return nil
@@ -39,10 +31,7 @@ func (user *User) Save() *errors.Response {
 	user.CreatedOn = dates.GetNowString()
 	err := users.DB.QueryRow(queryInsertUser, user.FirstName, user.LastName, user.Email, user.CreatedOn).Scan(&user.Id)
 	if err != nil {
-		if strings.Contains(err.Error(), errorUniqueEmail) {
-			return errors.Conflict(fmt.Sprintf("email %s is already registered", user.Email))
-		}
-		return errors.InternalServerError(err.Error())
+		return postgres.ParseError(err)
 	}
 
 	return nil
---
+++ utils/postgres/utils.go
@@ -0,0 +1,29 @@
+package postgres
+
+import (
+	"strings"
+
+	"github.com/lib/pq"
+	"github.com/regul4rj0hn/bookstore-users-api/utils/errors"
+)
+
+const (
+	ErrorEmptyResultSet = "no rows in result set"
+)
+
+func ParseError(err error) *errors.Response {
+	sqlErr, ok := err.(*pq.Error)
+	if !ok {
+		if strings.Contains(err.Error(), ErrorEmptyResultSet) {
+			return errors.NotFound("no record matching that ID")
+		}
+		return errors.InternalServerError("error parsing database server response")
+	}
+
+	switch sqlErr.Code {
+	case "23505":
+		return errors.Conflict("duplicate key")
+	}
+
+	return errors.InternalServerError("error when processing the request")
+}
