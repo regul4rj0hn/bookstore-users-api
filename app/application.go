ea767cf34c2ac15dfcfb323566b8b9d50953fe7c
Mauro Lavigna <mauro.lavigna@gmail.com>
2021-04-28 16:11:37 -0300

    add logger module

--- app/application.go
+++ app/application.go
@@ -1,6 +1,9 @@
 package app
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/regul4rj0hn/bookstore-users-api/logger"
+)
 
 var (
 	router = gin.Default()
@@ -8,5 +11,6 @@ var (
 
 func StartApplication() {
 	mapUrls()
+	logger.Info("users service API starting...")
 	router.Run()
 }
